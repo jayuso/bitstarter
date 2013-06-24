45f867324e41c5375e05aec56a1e9ddf097f8c89
Javier Ayuso <javier.ayuso@gmail.com>
2013-06-24 21:05:50 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require('express');
 
-var app = express.createServer(express.logger());
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "Modify this script to write out something different.\n";
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
