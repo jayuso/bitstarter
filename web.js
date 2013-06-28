c852383881af5a9e096516485a270689dd74a07b
Javier Ayuso <javier.ayuso@gmail.com>
2013-06-28 20:35:46 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,5 +3,4 @@ var fs = require('fs');
 var outfile = "hello.txt";
 var out = "Modify this script to write out something different.\n";
 fs.writeFileSync(outfile, out);  
-//console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
-console.log("Test");
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
